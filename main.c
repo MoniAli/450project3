3ca4324e60739f5c0b97a473d439492033f41eef
Moni Ali <MoniAli@Macintosh-2.local>
2017-10-30 14:15:56 -0400

    Main

---
+++ main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  
+//
+//  Created by Moni Ali on 10/30/17.
+//
+//
+
+#include "main.h"
+
+int main(int argc,char** argv){
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  
+//
+//  Created by Moni Ali on 10/30/17.
+//
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
