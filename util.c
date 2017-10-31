cca6f4f5f5cd474c9bed6bd1b87a286f10568417
Moni Ali <MoniAli@Macintosh-2.local>
2017-10-31 17:34:02 -0400

    format

--- util.c
+++ util.c
@@ -14,7 +14,7 @@ int8_t format(char* fileName){
     fp = fopen(fileName, "w+");
     
     char data[NUM_SECTORS * SECTOR_SIZE];
-    for (int i = 0; i < sizeof(NUM_SECTORS * SECTOR_SIZE); i++){
+    for (int i = 0; i < NUM_SECTORS * SECTOR_SIZE; i++){
         printf("%d", i);
         data[i] = 'x';
     }
