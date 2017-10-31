22a6769bf39b6a374cefa3f1f5aaccb78b9898e4
Moni Ali <MoniAli@Macintosh-2.local>
2017-10-31 14:44:03 -0400

    format

--- main.c
+++ main.c
@@ -7,8 +7,22 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "util.h"
 
 int main(int argc,char** argv){
-    format("Hello");
+    uint8_t response;
+    while (true):
+        printf("Menu of options:\n");
+        printf("    1) Format\n");
+        printf("    14) Exit the program\n");
+    
+    
+        get_input(&response);
+        printf("%d", response);
+    
+}
+
+void get_input(){
+    scanf("%d", response);
 }
