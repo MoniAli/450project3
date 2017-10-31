6083c9756347a8e44e95ec079310235f0efebfa4
Moni Ali <MoniAli@Macintosh-2.local>
2017-10-31 14:59:23 -0400

    format

--- main.c
+++ main.c
@@ -15,30 +15,35 @@ void get_input(int8_t *response){
     scanf("%hhd", response);
 }
 
+void print_menu(){
+    printf("Menu of options:\n");
+    printf("    1) Format\n");
+    printf("    14) Exit the program\n");
+}
+
+void handle_response(uint8_t response){
+    switch(response){
+        case 1:
+            format("Hello");
+            response = 0;
+            break;
+        case 14:
+            return 0;
+            break;
+        default:
+            printf("Invalid response, try again\n");
+            response = 0;
+            break;
+    }
+    
+}
 
 int main(int argc,char** argv){
     uint8_t response;
     while (true){
-        printf("Menu of options:\n");
-        printf("    1) Format\n");
-        printf("    14) Exit the program\n");
-    
-    
+        print_menu();
         get_input(&response);
-        switch(response){
-            case 1:
-                format("Hello");
-                response = 0;
-                break;
-            case 14:
-                return 0;
-                break;
-            default:
-                printf("Invalid response, try again\n");
-                response = 0;
-                break;
-                
-        }
+        handle_response(response);
     }
     
 }
