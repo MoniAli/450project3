f86e45311a861bae64544d687387116a2b05ce5f
Moni Ali <MoniAli@Macintosh-2.local>
2017-10-31 16:45:39 -0400

    format

--- util.c
+++ util.c
@@ -9,10 +9,11 @@
 #include "util.h"
 
 int8_t format(){
-    char fileName[50] = {0};
+    char* fileName = malloc(sizeof(char)*100);
     
     printf("Please enter the name of the file that contains the file system ");
-    scanf("%49s", fileName);
+    scanf("%99s", fileName);
     printf("%s is the filename\n", fileName);
+    free(fileName);
     return 0;
 }
