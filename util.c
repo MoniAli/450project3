ab9c722f03be51405fc26aaae902e069590e03df
Moni Ali <MoniAli@Macintosh-2.local>
2017-10-31 14:24:04 -0400

    format

---
+++ util.c
@@ -0,0 +1,13 @@
+//
+//  util.c
+//  
+//
+//  Created by Moni Ali on 10/31/17.
+//
+//
+
+#include "util.h"
+
+int8_t format(char* fileName){
+    return -1
+}
---
+++ util.h
@@ -0,0 +1,15 @@
+//
+//  util.h
+//  
+//
+//  Created by Moni Ali on 10/31/17.
+//
+//
+
+#ifndef util_h
+#define util_h
+
+#include <stdio.h>
+#include <stint.h>
+
+#endif /* util_h */
