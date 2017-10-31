ee5f0f508277b2f9f344a11b1f81b6ca2322f7c1
Moni Ali <MoniAli@Macintosh-2.local>
2017-10-31 17:38:12 -0400

    format

--- util.c
+++ util.c
@@ -16,7 +16,7 @@ int8_t format(char* fileName){
     char data[NUM_SECTORS * SECTOR_SIZE];
     for (int i = 0; i < NUM_SECTORS * SECTOR_SIZE; i++){
         printf("%d", i);
-        data[i] = 'x';
+        data[i] = itoa(i);
     }
     printf("%ld", sizeof(*data));
     
