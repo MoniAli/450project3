92b9c62dfc716b0d424b956797b8b6a9045aa2b7
Moni Ali <MoniAli@Macintosh-2.local>
2017-11-02 13:46:59 -0400

    format

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void get_input(uint8_t *response){
     bool alpha = false;
     int8_t i = 0;
     while (buffer[i] != '\0' && i != 10){
-        printf("%c ", buffer[i]);
+        printf("%c |", buffer[i]);
         if (!isdigit(buffer[i])){
             alpha = true;
         }
