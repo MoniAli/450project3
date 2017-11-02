f62e6d71a079470ad27b5e67c8bf7568f02228c3
Moni Ali <MoniAli@Macintosh-2.local>
2017-11-02 16:26:51 -0400

    format

--- util.c
+++ util.c
@@ -17,6 +17,7 @@ int8_t format(char* fileName){
     sb.magic = 0xDEADBEEF;
     sb.size = 1;
     sb.inode_count = 9;
+    sb.datablock_count = 246;
     *sb.inode_stats = malloc(sizeof(char)*144);
     *sb.datablock_stats = malloc(sizeof(char)*246);
     for (i = 0; i < 144; i++){
@@ -26,7 +27,12 @@ int8_t format(char* fileName){
         sb.datablock_stats[i] = 0;
     }
     
+    fwrite(&sb.magic, 4, 1, fp);
+    fwrite(&sb.size, 4, 1, fp);
+    fwrite(&sb.inode_count, 4, 1, fp);
+    fwrite(&sb.datablock_count, 4, 1, fp);
     fwrite(sb.inode_stats, 1, 144, fp);
+    fwrite(sb.datablock_stats, 1, 246, fp);
     
    // fwrite(data, 1, NUM_SECTORS * SECTOR_SIZE, fp);
 
